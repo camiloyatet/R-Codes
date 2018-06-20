c4d5637fda1f11208a9611f29cc143f164f9f2bb
Camilo Yate <camilo3.14159265@gmail.com>
2018-06-20 11:28:04 -0500

    Create ConcatenateStrings.r

---
+++ ConcatenateStrings.r
@@ -0,0 +1,21 @@
+Unir.Cadenas <- function(..., sep = " ", collapse = NULL, na.rm = F) {
+    if (!na.rm)
+        paste(..., sep = sep, collapse = collapse)
+    else
+        if (na.rm == T) {
+            paste.na <- function(x, sep) {
+                x <- gsub("^\\s+|\\s+$", "", x)
+                ret <- paste(na.omit(x), collapse = sep)
+                is.na(ret) <- ret == ""
+                return(ret)
+            }
+            df <- data.frame(..., stringsAsFactors = F)
+            ret <- apply(df, 1, FUN = function(x) paste.na(x, sep))
+            
+            if (is.null(collapse))
+                ret
+            else {
+                paste.na(ret, sep = collapse)
+            }
+        }
+}
