1a8457a2233138f9768f8bbcc2d2518b5167f0b1
Camilo Yate <camilo3.14159265@gmail.com>
2018-07-19 09:27:45 -0500

    Update AppendFolder.R

--- AppendFolder.R
+++ AppendFolder.R
@@ -1,7 +1,7 @@
 # Carga en un solo dataset todos los archivos de cierta extension dentro de una caperta especificada
 
 Cargar.Datos<-function (carpeta, extension="csv", exhaustivo=F, n_ultimos=0, ordenado=T, ausentes=getOption("datatable.na.strings","NA"), 
-                        separador="auto", dec=".", quote="\"", header="auto"){
+                        separador="auto", dec=".", quote="\"", header="auto", clases=NULL){
     
     if(ordenado) file.list <- paste0(carpeta,"/",list.files(carpeta, pattern = paste0("+.",extension), recursive = F))
     else file.list <- sort(paste0(carpeta,"/",list.files(carpeta, pattern = paste0("+.",extension), recursive = F)))
@@ -16,7 +16,8 @@ Cargar.Datos<-function (carpeta, extension="csv", exhaustivo=F, n_ultimos=0, ord
                      lapply(file.list, FUN = function(file) {
                          fread(file, sep=separador, dec=dec, quote=quote, header = header,
                                na.strings = ausentes,
-                               col.names = names(fread(file.list[1], nrows = 1))
+                               col.names = names(fread(file.list[1], nrows = 1)),
+                               colClasses = clases
                          )}
                      )
     )
