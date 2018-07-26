745e7e206902c6f3844f22323e75b0d56619bef1
Camilo Yate <camilo3.14159265@gmail.com>
2018-07-26 10:16:44 -0500

    Update AppendFolder.R

--- AppendFolder.R
+++ AppendFolder.R
@@ -1,8 +1,10 @@
 # Carga en un solo dataset todos los archivos de cierta extension dentro de una caperta especificada
 
-Cargar.Datos<-function (carpeta, extension="csv", exhaustivo=F, n_ultimos=0, ordenado=T, ausentes=getOption("datatable.na.strings","NA"), 
+Cargar.Datos<-function (carpeta, extension="csv", exhaustivo=F, Fuente=T, n_ultimos=0, ordenado=T, ausentes=getOption("datatable.na.strings","NA"), 
                         separador="auto", dec=".", quote="\"", header="auto", clases=NULL){
     
+    require("dplyr"); require("data.table"); require("qdapRegex")
+  
     if(ordenado) file.list <- paste0(carpeta,"/",list.files(carpeta, pattern = paste0("+.",extension), recursive = F))
     else file.list <- sort(paste0(carpeta,"/",list.files(carpeta, pattern = paste0("+.",extension), recursive = F)))
     m=length(file.list)
@@ -12,14 +14,17 @@ Cargar.Datos<-function (carpeta, extension="csv", exhaustivo=F, n_ultimos=0, ord
     
     print("Importando:: ")
     print(file.list)
-    Union <- do.call("rbind",
+    Union <- do.call("bind_rows",
                      lapply(file.list, FUN = function(file) {
                          fread(file, sep=separador, dec=dec, quote=quote, header = header,
                                na.strings = ausentes,
                                col.names = names(fread(file.list[1], nrows = 1)),
                                colClasses = clases
-                         )}
+                         ) %>% mutate(Source=as.character(rm_between(gsub(fold, "", file), "/", ".", extract=TRUE)[[1]]))
+                       }
                      )
     )
+    if(!Fuente)
+      Union<-Union %>% select(-Source)
     return(Union)
 }
