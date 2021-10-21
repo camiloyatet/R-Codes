3aeb68ef73230927b42be849ffcd7ea46fbd188a
Camilo Yate <camilo3.14159265@gmail.com>
2021-10-21 10:26:15 -0500

    Update and rename AppendFolder.R to CargarDatos.R

--- CargarDatos.R
+++ CargarDatos.R
@@ -1,12 +1,12 @@
 # Carga en un solo dataset todos los archivos de cierta extension dentro de una caperta especificada
 
 Cargar.Datos<-function (carpeta, extension="csv", exhaustivo=F, Fuente=T, n_ultimos=0, ordenado=T, ausentes=getOption("datatable.na.strings","NA"), 
-                        separador="auto", dec=".", quote="\"", header="auto", clases=NULL){
+                        separador="auto", dec=".", quote="\"", header="auto", clases=NULL){                    
+                            
+    require("dplyr"); require("data.table"); require(qdapRegex)
     
-    require("dplyr"); require("data.table"); require("qdapRegex")
-  
-    if(ordenado) file.list <- paste0(carpeta,"/",list.files(carpeta, pattern = paste0("+.",extension), recursive = F))
-    else file.list <- sort(paste0(carpeta,"/",list.files(carpeta, pattern = paste0("+.",extension), recursive = F)))
+    if(ordenado) file.list <- paste0(carpeta,"/",list.files(carpeta, pattern = paste0("+.",extension), recursive = exhaustivo))
+    else file.list <- sort(paste0(carpeta,"/",list.files(carpeta, pattern = paste0("+.",extension), recursive = exhaustivo)))
     m=length(file.list)
     n=ifelse((n_ultimos>=m| n_ultimos<1),1, m-(n_ultimos-1))
     
@@ -18,9 +18,9 @@ Cargar.Datos<-function (carpeta, extension="csv", exhaustivo=F, Fuente=T, n_ulti
                      lapply(file.list, FUN = function(file) {
                          fread(file, sep=separador, dec=dec, quote=quote, header = header,
                                na.strings = ausentes,
-                               col.names = names(fread(file.list[1], nrows = 1)),
+                               col.names = Limpiar.Cadenas(names(fread(file.list[1], nrows = 1))),
                                colClasses = clases
-                         ) %>% mutate(Source=as.character(rm_between(gsub(fold, "", file), "/", ".", extract=TRUE)[[1]]))
+                         ) %>% mutate(Source=as.character(rm_between(gsub(carpeta, "", file), "/", ".", extract=TRUE)[[1]]))
                        }
                      )
     )
