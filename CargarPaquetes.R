a0890db0b54d101499c0028b515c8f31d4775fdf
Camilo Yate <camilo3.14159265@gmail.com>
2021-10-21 10:27:57 -0500

    Update and rename Load Packages.R to CargarPaquetes.R

--- CargarPaquetes.R
+++ CargarPaquetes.R
@@ -1,6 +1,6 @@
-Loadpkg <- function(pkg){
+CargarPaquetes <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
   if (length(new.pkg)) 
-    install.packages(new.pkg, dependencies = TRUE)
+    install.packages(new.pkg, dependencies = T)
   sapply(pkg, require, character.only = TRUE)
 }
