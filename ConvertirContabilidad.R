33dd1d0574942b422b38579f731ec2104bffefb0
Camilo Yate <camilo3.14159265@gmail.com>
2021-10-21 10:28:37 -0500

    Update and rename Convert Account Format.R to ConvertirContabilidad.R

--- ConvertirContabilidad.R
+++ ConvertirContabilidad.R
@@ -1,4 +1,4 @@
-convertir.contabilidad <- function(x){
+ConvertirContabilidad <- function(x){
   if(grepl("\\(.*\\)", x)){
     as.numeric(paste0("-", gsub("\\(|\\)", "", gsub("[\\$, ]", "", x))))
   } else {
