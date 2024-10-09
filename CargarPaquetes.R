ff8f1ffe29c643718c02f5b9b1756ab47fa6f20c
Camilo Yate <camilo3.14159265@gmail.com>
2024-10-09 07:26:21 -0500

    Delete CargarPaquetes.R

--- CargarPaquetes.R
+++
@@ -1,6 +0,0 @@
-CargarPaquetes <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg)) 
-    install.packages(new.pkg, dependencies = T)
-  sapply(pkg, require, character.only = TRUE)
-}
